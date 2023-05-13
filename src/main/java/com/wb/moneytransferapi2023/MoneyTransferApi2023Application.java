eee255755cd0ef93d30d20e4022eb14772dd424e
samuel debebe <samuel.debebe.27@gmail.com>
2023-05-13 19:27:05 +0300

    Money transfer API application saved on local repository

---
+++ src/main/java/com/wb/moneytransferapi2023/MoneyTransferApi2023Application.java
@@ -0,0 +1,13 @@
+package com.wb.moneytransferapi2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferApi2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTransferApi2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wb/moneytransferapi2023/MoneyTransferApi2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wb.moneytransferapi2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferApi2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
